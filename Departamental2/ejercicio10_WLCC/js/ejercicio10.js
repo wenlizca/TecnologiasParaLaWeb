1d68adf5f98c7d42fa2dba7a0bd8bac522df6bd4
wenlizca <47064887+wenlizca@users.noreply.github.com>
2021-06-18 17:24:19 -0500

    ejercicio10_WLCC

---
+++ Departamental2/ejercicio10_WLCC/js/ejercicio10.js
@@ -0,0 +1,34 @@
+function mostrarPrimario()
+{
+	//$('#div-info').show();
+	$('#div-primario').toggle(500); //muestro mediante id
+}
+
+function mostrarSecundario()
+{
+	//$('#div-info').show());
+	$('#div-secundario').toggle(500); //muestro mediante id
+}
+
+function mostrarExito()
+{
+	//$('#div-info').show();
+	$('#div-exito').toggle(500); //muestro mediante id
+}
+
+function mostrarInfo()
+{
+	//$('#div-info').show();
+	$('#div-info').toggle(500); //muestro mediante id
+}
+
+function mostrarPeligro()
+{
+	//$('#div-info').show());
+	$('#div-peligro').toggle(500); //muestro mediante id
+}
+function mostrarPrecausion()
+{
+	//$('#div-info').show();
+	$('#div-precausion').toggle(500); //muestro mediante id
+}
